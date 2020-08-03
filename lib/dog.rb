9cf668f2ce7d4c3809595ce0bf931ddd24967a15
Dong Xia <dong7777xia@gmail.com>
2020-08-03 17:00:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
 
   def name= (dog_name)
-    this_dog_name = dog_name
+    @this_dog_name = dog_name
   end
 
   def name
-    this_dog_name
+    @this_dog_name
   end
 
 end
@@ -13,3 +13,5 @@ end
 
 lassie = Dog.new
 lassie.name = "Lassie"
+
+puts lassie.name
